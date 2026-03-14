63c7cadb75679623e7be60b1a1ad0329ec93a3b6
Naman Choudhary <namanchoudhary114@gmail.com>
2026-03-14 22:03:48 +0530

    changes in the first.js

--- first.js
+++ first.js
@@ -5,3 +5,32 @@ a=null
 y=undefined
 isFollow=true
 console.log(name);
+
+
+var age=40;
+var age=50;
+console.log(age);
+
+{
+    let a=10;
+    console.log(a);
+    
+}
+{
+    let a=6;
+console.log(a);
+
+}
+
+const student={
+    fullName:"Rahul Kumar",
+    age:27,
+    cgpa:6,
+    isPass:true
+}
+console.log(student.fullName);
+console.log(student[age]);
+
+
+student["fullName"] = "Rahul Sharma"
+console.log(student["fullName"])
